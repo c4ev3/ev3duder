096602f426d111c47ee60f91fd5ea8673b621314
Ahmad Fatoum <ahmad@a3f.at>
2015-06-27 17:03:12 +0200

    documented test.c

--- src/test.c
+++ src/test.c
@@ -14,14 +14,21 @@
 #include <sys/utsname.h>
 #endif
 
+//! Full HID packet for causing a beep
 static const u8 tone[] = "\x0\x0F\x00\0\0\x80\x00\x00\x94\x01\x81\x02\x82\xE8\x03\x82\xE8\x03";
 
 #define MAX_STR 256
 
+/**
+ * \return err_code An error code according to \p enum ERR
+ * \brief cause a beep and print HID information if applicable
+ * \bug needs more detailed information for bluetooth/wlan
+ * 		should print COM port, device name
+ */ 
 int test()
 {
     int res;
-    //TODO: needs more detailed information for bluetooth/wlan
+    //TODO: 
     res = ev3_write(handle, tone, sizeof tone - 1);
     if (res < 0)
     {
@@ -36,6 +43,7 @@ int test()
     struct utsname utsname;
     uname(&utsname);
     int isOld = strcmp(utsname.release, "2.6.24") < 0;
+//! Print udev rules
     printf("SUBSYSTEM==\"%s\", ATTRS{idVendor}==\"0694\", ATTRS{idProduct}==\"0005\a, MODE=\"0666\"\n", isOld ? "usb_device" : "usb");
 #endif
     wchar_t wstr[MAX_STR];
