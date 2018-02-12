b3ad7c4f6ea53d95a926aadf6f7850fd7c225ef5
daniel mueller <danielm-hab@dnml.de>
2018-02-12 14:34:59 +0100

    Fixed upload bug.
    Fixed bug where an error occurs when trying to upload files with a size that is a multiple of CHUNK_SIZE (1000).
    
    Signed-off-by: Ahmad Fatoum <ahmad@a3f.at>

--- src/up.c
+++ src/up.c
@@ -34,17 +34,25 @@ int up(FILE *fp, const char *dst)
 	long fsize = ftell(fp);
 	fseek(fp, 0, SEEK_SET);
 
-	if ((unsigned long) fsize > (u32) -1)
+	if ((unsigned long) fsize > UINT32_MAX)
 		return ERR_FTOOBIG;
 
-	unsigned chunks = fsize / CHUNK_SIZE;
-	unsigned final_chunk_sz = fsize % CHUNK_SIZE;
+	unsigned long final_chunk_sz = (unsigned long)(fsize % CHUNK_SIZE);
+	unsigned long chunks = (unsigned long)(fsize / CHUNK_SIZE);
+
+	// final_chunk_sz == 0 ==> fsize is a multiple of CHUNK_SIZE
+	// So the final_chunk_sz is supposed CHUNK_SIZE and not 0
+	// If final_chunk_sz != 0 add one to chunks, so we can send the final packet in the loop
+	if (final_chunk_sz == 0)
+		final_chunk_sz = CHUNK_SIZE;
+	else
+		chunks++;
 
 	//fprintf(stderr, "Attempting file upload (%ldb total; %u chunks): \n", fsize, chunks + 1);
 
 	size_t dst_len = strlen(dst) + 1;
 	BEGIN_DOWNLOAD *bd = packet_alloc(BEGIN_DOWNLOAD, dst_len);
-	bd->fileSize = fsize;
+	bd->fileSize = (u32)fsize;
 	memcpy(bd->fileName, dst, dst_len);
 
 	res = ev3_write(handle, (u8 *) bd, bd->packetLen + PREFIX_SIZE);
@@ -69,11 +77,17 @@ int up(FILE *fp, const char *dst)
 		return ERR_VM;
 	}
 
+
 	CONTINUE_DOWNLOAD *cd = packet_alloc(CONTINUE_DOWNLOAD, CHUNK_SIZE);
-	for (size_t i = 0; i < chunks; ++i)
+	for (size_t i = 0; i < chunks; i++)
 	{
 		cd->fileHandle = bdrep.fileHandle;
-		fread(cd->fileChunk, 1, CHUNK_SIZE, fp);
+		// If i == chunks-1 (if this is the final chunk), use the proper packetLen
+		if (i == chunks-1)
+			cd->packetLen = sizeof(CONTINUE_DOWNLOAD) + final_chunk_sz - PREFIX_SIZE;
+
+		// If i == chunks-1 (if this is the final chunk), use final_chunk_sz instead of CHUNK_SIZE
+		fread(cd->fileChunk, 1, (i == chunks-1) ? final_chunk_sz : CHUNK_SIZE, fp);
 		res = ev3_write(handle, (u8 *) cd, cd->packetLen + PREFIX_SIZE);
 
 		if (res < 0)
@@ -81,6 +95,7 @@ int up(FILE *fp, const char *dst)
 			errmsg = "Unable to write CONTINUE_DOWNLOAD.";
 			return ERR_COMM;
 		}
+
 		res = ev3_read_timeout(handle, (u8 *) &bdrep, sizeof bdrep, TIMEOUT);
 		if (res <= 0)
 		{
@@ -88,6 +103,9 @@ int up(FILE *fp, const char *dst)
 			return ERR_COMM;
 		}
 	}
+
+
+#if 0
 	cd->packetLen = sizeof(CONTINUE_DOWNLOAD) + final_chunk_sz - PREFIX_SIZE;
 	fread(cd->fileChunk, 1, final_chunk_sz, fp);
 	res = ev3_write(handle, (u8 *) cd, cd->packetLen + PREFIX_SIZE);
@@ -96,12 +114,15 @@ int up(FILE *fp, const char *dst)
 		errmsg = "Unable to write CONTINUE_DOWNLOAD.";
 		return ERR_COMM;
 	}
+
 	res = ev3_read_timeout(handle, (u8 *) &bdrep, sizeof bdrep, TIMEOUT);
 	if (res <= 0)
 	{
 		errmsg = "Unable to read CONTINUE_DOWNLOAD";
 		return ERR_COMM;
 	}
+#endif
+
 	if (bdrep.type == VM_ERROR)
 	{
 		errno = bdrep.ret;
