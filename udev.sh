a5893f4516cdf72c517c7200bb2df4b562142f23
David Lechner <david@lechnology.com>
2019-04-18 17:05:04 -0500

    Update udev rules file
    Rules always start with a prioity number. Also use the project name so
    that we know where this rule came from.
    
    Also add TAG+="uaccess" and a newline to the end of the file (some
    versions of udev won't handle file well if there are missing the
    final newline).

--- udev.sh
+++ udev.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # too clever for its own good..
 #eval "./ev3duder test 2>/dev/null | head -1 > /etc/udev/rules.d"
-sudo cp ev3-udev.rules /etc/udev/rules.d/ev3.rules
+sudo cp 99-ev3duder.rules /etc/udev/rules.d/
 
 sudo udevadm control --reload-rules
 sudo udevadm trigger
