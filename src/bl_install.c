25ce74cf59468201bda726e0770c1a7acf2c4813
Jakub Vaněk <vanek.jakub4@seznam.cz>
2020-08-13 12:35:22 +0200

    feat(flash): implement flash install sub-operations

--- src/bl_install.c
+++ src/bl_install.c
@@ -94,23 +94,168 @@ int bootloader_install(FILE *fp)
 
 static int bootloader_erase(void)
 {
-	return ERR_VM;
+	FW_ERASEFLASH *request = packet_alloc(FW_ERASEFLASH, 0);
+	int res = ev3_write(handle, (u8 *) request, request->packetLen + PREFIX_SIZE);
+	if (res < 0)
+	{
+		errmsg = "Unable to write FW_ERASEFLASH.";
+		return ERR_COMM;
+	}
+
+	FW_ERASEFLASH_REPLY *reply = malloc(sizeof(FW_ERASEFLASH_REPLY));
+	res = ev3_read_timeout(handle, (u8 *) reply, sizeof(FW_ERASEFLASH_REPLY), -1);
+	if (res <= 0)
+	{
+		errmsg = "Unable to read FW_ERASEFLASH";
+		return ERR_COMM;
+	}
+
+	if (reply->type != VM_OK)
+	{
+		errno = reply->ret;
+		fputs("Operation failed.\nlast_reply=", stderr);
+		print_bytes(reply, reply->packetLen + 2);
+
+		errmsg = "`FW_ERASEFLASH` was denied.";
+		return ERR_VM;
+	}
+	return ERR_UNK;
 }
 
 static int bootloader_start(int offset, int length)
 {
-	(void) offset, (void) length;
-	return ERR_VM;
+	FW_START_DOWNLOAD *request = packet_alloc(FW_START_DOWNLOAD, 0);
+	request->flashStart = offset;
+	request->flashLength = length;
+	int res = ev3_write(handle, (u8 *) request, request->packetLen + PREFIX_SIZE);
+	if (res < 0)
+	{
+		errmsg = "Unable to write FW_START_DOWNLOAD.";
+		return ERR_COMM;
+	}
+
+	FW_START_DOWNLOAD_REPLY *reply = malloc(sizeof(FW_START_DOWNLOAD_REPLY));
+	res = ev3_read_timeout(handle, (u8 *) reply, sizeof(FW_START_DOWNLOAD_REPLY), -1);
+	if (res <= 0)
+	{
+		errmsg = "Unable to read FW_START_DOWNLOAD";
+		return ERR_COMM;
+	}
+
+	if (reply->type != VM_OK)
+	{
+		errno = reply->ret;
+		fputs("Operation failed.\nlast_reply=", stderr);
+		print_bytes(reply, reply->packetLen + 2);
+
+		errmsg = "`FW_START_DOWNLOAD` was denied.";
+		return ERR_VM;
+	}
+	return ERR_UNK;
 }
 
 static int bootloader_send(FILE *fp, int length, u32* pCrc32)
 {
-	(void) fp, (void) length, (void) pCrc32;
-	return ERR_VM;
+	*pCrc32 = 0;
+
+	int max_payload = 1024 - (sizeof(FW_DOWNLOAD_DATA) - PREFIX_SIZE + 2);
+	FW_DOWNLOAD_DATA *request = packet_alloc(FW_DOWNLOAD_DATA, max_payload);
+	FW_DOWNLOAD_DATA_REPLY *reply = malloc(sizeof(FW_DOWNLOAD_DATA_REPLY));
+
+	int total = length;
+	int sent_so_far = 0;
+	int file_ended = 0;
+	int res = 0;
+	int last_percent = 0;
+	u32 crc = 0;
+
+	printf("Progress: %3d %%\n", 0);
+
+	while (sent_so_far < total) {
+		int remaining = total - sent_so_far;
+		int this_block = remaining <= max_payload ? remaining : max_payload;
+
+		if (!file_ended) {
+			int real = fread(request->payload, 1, this_block, fp);
+			if (real < this_block) {
+				file_ended = 1;
+				memset(request->payload + real, 0, this_block - real);
+			}
+		} else {
+			memset(request->payload, 0, this_block);
+		}
+
+		crc = crc32(crc, request->payload, this_block);
+
+		request->packetLen = sizeof(FW_DOWNLOAD_DATA) - PREFIX_SIZE + this_block;
+
+		res = ev3_write(handle, (u8 *) request, request->packetLen + PREFIX_SIZE);
+		if (res < 0)
+		{
+			errmsg = "Unable to write FW_DOWNLOAD_DATA.";
+			return ERR_COMM;
+		}
+
+		res = ev3_read_timeout(handle, (u8 *) reply, sizeof(FW_DOWNLOAD_DATA_REPLY), -1);
+		if (res <= 0)
+		{
+			errmsg = "Unable to read FW_DOWNLOAD_DATA";
+			return ERR_COMM;
+		}
+
+		if (reply->type != VM_OK)
+		{
+			errno = reply->ret;
+			fputs("Operation failed.\nlast_reply=", stderr);
+			print_bytes(reply, reply->packetLen + 2);
+
+			errmsg = "`FW_DOWNLOAD_DATA` was denied.";
+			return ERR_VM;
+		}
+
+		sent_so_far += this_block;
+
+		int new_percent = sent_so_far * 100 / total;
+		if (new_percent >= last_percent + 5) {
+			last_percent += ((new_percent - last_percent) / 5) * 5;
+			printf("Progress: %3d %%\n", last_percent);
+		}
+	}
+	*pCrc32 = crc;
+	return ERR_UNK;
 }
 
 static int bootloader_checksum(int offset, int length, u32 *pCrc32)
 {
-	(void) offset, (void) length, (void) pCrc32;
-	return ERR_VM;
+	*pCrc32 = 0;
+
+	FW_GETCRC32 *request = packet_alloc(FW_GETCRC32, 0);
+	request->flashStart = offset;
+	request->flashLength = length;
+	int res = ev3_write(handle, (u8 *) request, request->packetLen + PREFIX_SIZE);
+	if (res < 0)
+	{
+		errmsg = "Unable to write FW_GETCRC32.";
+		return ERR_COMM;
+	}
+
+	FW_GETCRC32_REPLY *reply = malloc(sizeof(FW_GETCRC32_REPLY));
+	res = ev3_read_timeout(handle, (u8 *) reply, sizeof(FW_GETCRC32_REPLY), -1);
+	if (res <= 0)
+	{
+		errmsg = "Unable to read FW_GETCRC32";
+		return ERR_COMM;
+	}
+
+	if (reply->type != VM_OK)
+	{
+		errno = reply->ret;
+		fputs("Operation failed.\nlast_reply=", stderr);
+		print_bytes(reply, reply->packetLen + 2);
+
+		errmsg = "`FW_GETCRC32` was denied.";
+		return ERR_VM;
+	}
+	*pCrc32 = reply->crc32;
+	return ERR_UNK;
 }
